8d577de96bfbb9b19b1cbcd26ea36e03c04acd0a
lewisyng <lewisyoung1306@yahoo.de>
2022-05-15 14:41:40 +0200

    add typography sizes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ## This is a UI Package to streamline the development of all sorts of websites
+
+---
+## List of Components
+### Typography
--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -1,5 +1,29 @@
-export const Typography = () => {
+import './scss/main.scss';
+import cn from 'classnames';
+import { Color, Size, Variant } from './Typography.types';
+
+export const Typography = ({
+    children,
+    variant,
+    size = 'base',
+    bold,
+    color,
+}: {
+    children: React.ReactNode;
+    variant?: Variant;
+    size?: Size;
+    bold?: boolean;
+    color?: Color;
+}) => {
+    const Typography = variant || 'p';
+
     return (
-        <h1>This is Typoaa ddd </h1>
-    )
-}
+        <Typography
+            data-size={size}
+            data-color={color}
+            className={cn('typography', bold && 'typography__bold')}
+        >
+            {children}
+        </Typography>
+    );
+};
